14943faae621399f3e5a01ff11632e9e1be2ec47
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-06-30 00:33:40 +0400

    Add Server

---
+++ server/controllers/posts.js
@@ -0,0 +1,27 @@
+import PostMessage from "../models/postModels.js";
+
+// Getting the Post
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+// Create a new Post
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import postRoutes from "./routes/posts.js";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// routes
+app.use("/posts", postRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Connected to db, Server running on port: ${PORT}`)
+    )
+  )
+  .catch((err) => console.log(err.message));
